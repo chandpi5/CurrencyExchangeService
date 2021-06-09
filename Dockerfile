cf33fc222a617ef98794e0bed7b38b2211ce643c
Chandran Arumugam <chandranatdpi@gmail.com>
2021-06-09 08:04:15 +0530

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM openjdk:8-jdk-alpine
 ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
+COPY ${JAR_FILE} /usr/app/app.jar
+WORKDIR /usr/app/
+ENTRYPOINT ["java","-jar","app.jar"]
--- src/test/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
+++ src/test/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -3,10 +3,11 @@ package com.in28minutes.microservices.currencyconversionservice;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=CurrencyConversionServiceApplicationTests.class)
 public class CurrencyConversionServiceApplicationTests {
 
 	@Test
